261412477b71c2a7b803f9ca5c87c500d15901ea
sarahandrew1012 <sarah.andrew1012@gmail.com>
2025-11-29 20:38:07 -0500

    Added comments & fixed logic with EP.

--- interrupts_student1_student2_EP.cpp
+++ interrupts_student1_student2_EP.cpp
@@ -1,31 +1,32 @@
 /**
  * @file interrupts.cpp
  * @author Sasisekhar Govind
+ * @author Sarah Andrew, 101303468
  * @brief template main.cpp file for Assignment 3 Part 1 of SYSC4001
  * 
  */
 #include "interrupts_student1_student2.hpp"
 
-struct IO {
-    PCB process;
-    unsigned int io; // remaining I/O time
-};
-
+// This sorts by the high PID comes before the lower PID.
 void EP(std::vector<PCB> &ready_queue) {
-    std::sort(
-        ready_queue.begin(),
-        ready_queue.end(),
-        [](const PCB &a, const PCB &b) {
-            return a.PID > b.PID;   // smallest PID ends at back()
+    std::sort(ready_queue.begin(), ready_queue.end(),
+        [](const PCB &first, const PCB &second) {
+            return (first.PID > second.PID);
         }
     );
 }
+
+// Keeps track of the IO events by storing the process and completion time.
+struct IOEvent {
+    PCB process;
+    unsigned int io_completion;
+};
 
 std::tuple<std::string /* add std::string for bonus mark */ >
 run_simulation(std::vector<PCB> list_processes) {
 
     std::vector<PCB> ready_queue;   //The ready queue of processes
-    std::vector<IO>  wait_queue;    //The wait queue of processes
+    std::vector<IOEvent>  wait_queue;    //The wait queue of processes
     std::vector<PCB> job_list;      //A list to keep track of all the processes. This is similar
                                     //to the "Process, Arrival time, Burst time" table that you
                                     //see in questions. You don't need to use it, I put it here
@@ -33,6 +34,7 @@ run_simulation(std::vector<PCB> list_processes) {
 
     unsigned int current_time = 0;
     PCB running;
+    unsigned int last_io = 0;
 
     //Initialize an empty running process
     idle_CPU(running);
@@ -42,7 +44,6 @@ run_simulation(std::vector<PCB> list_processes) {
     //make the output table (the header row)
     execution_status = print_exec_header();
 
-    unsigned int cpu_since_last_io = 0;
 
     //Loop while till there are no ready or waiting processes.
     //This is the main reason I have job_list, you don't have to use it.
@@ -70,96 +71,78 @@ run_simulation(std::vector<PCB> list_processes) {
 
         ///////////////////////MANAGE WAIT QUEUE/////////////////////////
         //This mainly involves keeping track of how long a process must remain in the wait queue
-        for (auto it = wait_queue.begin(); it != wait_queue.end();) {
-            if (it->io == current_time) {
-                execution_status += print_exec_status(
-                                                    current_time,
-                                                    it->process.PID,
-                                                    WAITING,
-                                                    READY
-        );
-
-                it->process.state = READY;
-                ready_queue.push_back(it->process);
-
-                it = wait_queue.erase(it);
+        for (int i = 0; i < (int)wait_queue.size(); ) {
+            if (wait_queue[i].io_completion == current_time) {
+                execution_status += print_exec_status(current_time, wait_queue[i].process.PID, WAITING,
+                                                    READY);
+                
+                // Updayes the process and places into the ready queue and then removes it from the wait queue.
+                wait_queue[i].process.state = READY;
+                ready_queue.push_back(wait_queue[i].process);
+                wait_queue.erase(wait_queue.begin() + i);
             }
             else {
-                ++it;
+                i++;
             }
         }
         /////////////////////////////////////////////////////////////////
 
         //////////////////////////SCHEDULER//////////////////////////////
-        if (running.state != RUNNING && !ready_queue.empty()) {
-
-            EP(ready_queue); // EP scheduler
-
-            run_process(running, job_list, ready_queue, current_time);
-            cpu_since_last_io = 0;
-
-            execution_status += print_exec_status(
-                current_time,
-                running.PID,
-                READY,
-                RUNNING
-            );
-        }
-
+        // Checks to see if its running. When its running, it takes a part of the CPU,
+        // sees the last I/O event and updates the job list
         if (running.state == RUNNING) {
 
             running.remaining_time--;
-            cpu_since_last_io++;
+            last_io++;
             sync_queue(job_list, running);
 
-            // TERMINATION
-            if (running.remaining_time == 0) {
+            // Process terminating
+            if (running.remaining_time <= 0) {
 
-                execution_status += print_exec_status(
-                    current_time + 1,
-                    running.PID,
-                    RUNNING,
-                    TERMINATED
-                );
+                execution_status += print_exec_status(current_time, running.PID,
+                    RUNNING, TERMINATED);
 
                 terminate_process(running, job_list);
                 idle_CPU(running);
             }
 
-            else if (running.io_freq > 0 &&
-                     cpu_since_last_io >= running.io_freq) {
+            // IO occuring in the process, which creates a new IO event
+            else if (running.io_freq > 0 && last_io >= running.io_freq) {
 
-                execution_status += print_exec_status(
-                    current_time + 1,
-                    running.PID,
-                    RUNNING,
-                    WAITING
-                );
+                execution_status += print_exec_status(current_time, running.PID,
+                    RUNNING, WAITING);
 
                 running.state = WAITING;
                 sync_queue(job_list, running);
 
-                IO entry;
-                entry.process = running;
-                entry.io = current_time + 1 + running.io_duration;
+                IOEvent entry_io;
+                entry_io.process = running;
+                entry_io.io_completion = current_time + running.io_duration;
 
-                wait_queue.push_back(entry);
-
+                wait_queue.push_back(entry_io);
+                last_io = 0;
                 idle_CPU(running);
-                cpu_since_last_io = 0;
             }
         }
 
+        // Picks the next availiable processs
+        if (running.state != RUNNING && !ready_queue.empty()) {
+
+            EP(ready_queue);
+            run_process(running, job_list, ready_queue, current_time);
+            last_io = 0;
+
+            // Displays status
+            execution_status += print_exec_status(current_time, running.PID, READY,
+                RUNNING);
+        }
+        // add the time
         current_time++;
     }
-
     //Close the output table
     execution_status += print_exec_footer();
-
     return std::make_tuple(execution_status);
 }
-
-
 int main(int argc, char** argv) {
 
     //Get the input file from the user
